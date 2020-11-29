1375b0c02753300586e4d70d652b41f4d9152d0c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-11-29 23:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const append = ["Milo", "Otis", "Garfield"]
 const prepend = ["Milo", "Otis", "Garfield"]
 
 const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
 
 const removeFirst = ["Milo", "Otis", "Garfield"]
-removeFirst.pop()
+removeFirst.shift()
